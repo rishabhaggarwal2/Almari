d09bbdfbc577a725f5104a97345c4a3b69944cda
rishabhaggarwal2 <rishabhaggarwal2@gmail.com>
2015-10-10 16:24:30 -0700

    Added Product Create Page

--- app/views/post.js
+++ app/views/post.js
@@ -9,6 +9,19 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
-
+.controller('View2Ctrl', ['$scope', function($scope) {
+	$scope.sizes= ["XS", "S", "M", "L", "XL","OS"];
+	$scope.genders = ["M","F"];
+	$scope.product = {};
+	$scope.categories = ["Party","Wedding","Prom", "Formal", "Cultural", 'Accessories'];
+	$scope.product.imageURL = "http://www.gap.com/webcontent/0009/935/338/cn9935338.jpg";
+	$scope.selectSize = function(size){
+		$scope.product.size = size;
+	};
+	$scope.selectGender = function(gender){
+		$scope.product.gender = gender;
+	};
+	$scope.selectCategory = function(category){
+		$scope.product.category = category;
+	};
 }]);
