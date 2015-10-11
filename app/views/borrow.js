092c34a557f05653e9d7b94cd8c34e2872db01ef
rishabhaggarwal2 <rishabhaggarwal2@gmail.com>
2015-10-11 02:58:10 -0700

    Finished Browse Page

--- app/views/borrow.js
+++ app/views/borrow.js
@@ -10,17 +10,31 @@ angular.module('myApp.borrow', ['ngRoute'])
 }])
 
 .controller('BorrowCtrl', ['$scope','ParseService', function($scope, ParseService) {
-	$scope.categories = ["Party","Wedding","Prom", "Formal", "Cultural", 'Accessories'];
+	$scope.categories = ["Party", "Formal", "Wedding", "Cultural", 'Accessories', "Prom",];
 
 	$scope.products = [];
 
 	$scope.showProducts = function() {
-    ParseService.findProducts({}, function(err, results) {
-      $scope.products = results;
-      $scope.$apply();
-    });
-  };
+	    ParseService.findProducts({}, function(err, results) {
+	      $scope.products = results;
+	      $scope.$apply();
+	    });
+	};
 
-  $scope.showProducts();
+
+
+  	$scope.showProducts();
+
+  	$scope.selectedCategory = "";
+  	$scope.categorySelected = "";
+  	$scope.selectPageCategory = function(category){
+		$scope.selectedCategory = category;
+		$scope.categorySelected = "pageSelected";
+		 ParseService.findProducts({category:category}, function(err, results) {
+	      $scope.products = results;
+	      $scope.$apply();
+	    });
+	};
+
 
 }]);
